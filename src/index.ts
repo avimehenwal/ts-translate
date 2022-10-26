5e63c89a2adbc2b06bb3a611076f823d777bcdb8
avimehenwal <avi.mehanwal@gmail.com>
2022-10-26 14:15:36 +0200

    feat: :lock: get secret from environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log(`Hello`);
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const GOOGLE_TRANSLATE_API_KEY = process.env.GOOGLE_TRANSLATE_API_KEY ?? 'undefined';
+console.log(GOOGLE_TRANSLATE_API_KEY);
