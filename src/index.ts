8d0e0863c3c66bf786a87b16b1cb17b0bd96de46
avimehenwal <avi.mehanwal@gmail.com>
2022-10-26 16:41:08 +0200

    feat: :bug: add data and google-translate service

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
 import * as dotenv from 'dotenv';
+import { items } from './data';
+
+import { v2 } from '@google-cloud/translate';
+const { Translate } = v2;
+
 dotenv.config();
 
-const GOOGLE_TRANSLATE_API_KEY = process.env.GOOGLE_TRANSLATE_API_KEY ?? 'undefined';
-console.log(GOOGLE_TRANSLATE_API_KEY);
+const projectId = process.env.GOOGLE_TRANSLATE_API_KEY ?? 'undefined';
+console.log(projectId);
+
+console.log(items.length);
+for (let item of items) {
+  console.dir(item?.title);
+}
+
+const translate = new Translate({ keyFilename: './src/keyFile.json' });
+
+const text = items[0]?.title;
+const target = `de`;
+
+(async () => {
+  let [translations] = await translate.translate(text, target);
+  console.dir(translations, { depth: null });
+})();
