16a1478b426833f8f6669fa7d4a1edd79900ad9f
avimehenwal <avi.mehanwal@gmail.com>
2022-10-31 14:17:43 +0100

    fix: :bug: use strict types for promise and error format

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
 import { items } from './data';
+import { IErrorFormat } from './service/translate';
 import { IItemResult, translateItem } from './service/translateItem';
 
 async function main() {
     const processedQueue = [];
-    const errorQueue = [];
+    const errorQueue: Array<IErrorFormat> = [];
 
     console.log(`INFO:: 🚧 translating total [${items.length}] items .....`);
     for (const item of items) {
         try {
             processedQueue.push(await translateItem(item));
         } catch (e) {
-            errorQueue.push(e);
+            errorQueue.push(e as IErrorFormat);
         }
     }
-    const successResult = await Promise.all<IItemResult>(processedQueue);
-    const failureResults = await Promise.all(errorQueue);
+    const successResult = await Promise.all<IItemResult[]>(processedQueue);
+    const failureResults = await Promise.all<IErrorFormat[]>(errorQueue);
     // console.dir(successResult)
 
     console.log(`INFO:: ✅ [${successResult.length}] translation completed`);
