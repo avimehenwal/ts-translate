bd7aa7b0c768bb56a1e1dcceea99688737feaed3
avimehenwal <avi.mehanwal@gmail.com>
2022-10-31 11:37:12 +0100

    refactor: :zap: decouple service into its own seperate module

---
+++ src/service/translate.ts
@@ -0,0 +1,38 @@
+type ITargetLanguage = `de` | `fr`;
+type ISourceLanguage = `en`;
+
+interface IErrorFormat {
+  code: number;
+  msg: string;
+}
+
+const TARGET_LANG: ITargetLanguage = `de`;
+const DELAY: number = 400;
+
+export function translateService(str: string, target: ITargetLanguage = TARGET_LANG): Promise<string> {
+  // several tranlsting straetgies could be used here. Eg: google translate service
+  // for my purposed I am mocked it
+
+  // const projectId = process.env.GOOGLE_TRANSLATE_API_KEY ?? 'undefined';
+  // const translate = new Translate({ keyFilename: './src/keyFile.json' });
+
+  // (async () => {
+  //   let [translations] = await translate.translate(text, TARGET_LANG);
+  //   console.dir(translations, { depth: null });
+  // })();
+
+  return new Promise((resolve, rejects) => {
+    setTimeout(() => {
+      resolve(`Mocked::${str}: - ${(Math.random() + 1).toString(36)}`);
+    }, DELAY);
+
+    // simulate a error path
+    if (str.includes("error")) {
+      const errorMsg: IErrorFormat = {
+        code: 500,
+        msg: `some custom error message`
+      }
+      rejects(errorMsg)
+    }
+  });
+}
